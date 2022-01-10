5e9cf38d85bd75c1f31c056b932bd3585084df2c
André Cordeiro <93953859+AndreCordeir0@users.noreply.github.com>
2022-01-10 08:52:21 -0300

    Add files via upload

---
+++ dark.js
@@ -0,0 +1,8 @@
+let $button = document.querySelector('button#dark-button');
+let $botao =document.querySelector('button i')
+let $letra=document.querySelector('p.dark-mode-p')
+$button.onclick = ()=>{
+  document.body.classList.toggle('dark')
+  
+
+}
